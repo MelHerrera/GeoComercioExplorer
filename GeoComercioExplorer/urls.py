221a26aff23cbf89fac80c2f0c706a86aab6e080
Melkin Herrera mendoza <melkinantonioh@gmail.com>
2024-07-17 23:26:08 -0600

    Configuracion de bootstrap icons y mejoras generales en el home

--- GeoComercioExplorer/urls.py
+++ GeoComercioExplorer/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from GeoComercioExplorer.views import HomePage
+from GeoComercioExplorer.views import HomePage, DashBoard
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePage),
+    path('dashboard', DashBoard)
+
 ]
--- GeoComercioExplorer/views.py
+++ GeoComercioExplorer/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def HomePage(request):
     return render(request, "home.html")
+
+def DashBoard(request):
+    # codigoPostal = codigoPostal if codigoPostal else 10
+    codigoPostal = 10
+    return render(request, "dashboard.html")
