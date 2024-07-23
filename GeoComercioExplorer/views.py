c0fad71bae75d8bf098a2ddb49b13e9b9af70f5c
Melkin Herrera mendoza <melkinantonioh@gmail.com>
2024-07-22 21:06:34 -0600

    Integración y Pruebas de Spark

--- GeoComercioExplorer/views.py
+++ GeoComercioExplorer/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from pyspark.sql import SparkSession
 
 def HomePage(request):
     return render(request, "home.html")
@@ -7,4 +8,25 @@ def HomePage(request):
 def DashBoard(request):
     # codigoPostal = codigoPostal if codigoPostal else 10
     codigoPostal = 10
-    return render(request, "dashboard.html")
+    ##instalar java https://www.oracle.com/java/technologies/downloads/?er=221886
+    ##agregar java a las variables de entorno path
+    ##Descargar los binarios de spark https://spark.apache.org/downloads.html
+    ##agregar spark a las variables de entorno 
+    spark = SparkSession.builder.appName("GeoApp").getOrCreate()
+
+    data_path = "GeoComercioExplorer\content\CPdescarga.csv"
+    df = spark.read.csv(data_path, header=True, inferSchema=True)
+    top_5_df = df.limit(5)
+
+    # Convertir a una lista de diccionarios para pasar a la plantilla
+    top_5_list = top_5_df.collect()
+    top_5_data = [row.asDict() for row in top_5_list]
+
+    # Parar SparkSession
+    spark.stop()
+
+    # Renderizar el resultado en una plantilla
+    return render(request, 'dashboard.html', {'top_5_data': top_5_data})
+
+# def initializeSpark(name):
+#     return SparkSession.builder.appName(name).getOrCreate()
