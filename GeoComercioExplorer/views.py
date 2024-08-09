5c6d3d32b763bcf2269a23debb3fec6bfd4c014e
Melkin Herrera mendoza <melkinantonioh@gmail.com>
2024-08-08 19:45:23 -0600

    Se agrego codigo para posteriormente integrar la visualizacion con google map

--- GeoComercioExplorer/views.py
+++ GeoComercioExplorer/views.py
@@ -214,40 +214,34 @@ def Get_actividades(request,codigo_postal, radio, actividad_to_search):
     comerciosB_SHP = comerciosB_SHP.set_crs('PROJCS["Mexico_ITRF2008_LCC",GEOGCS["Mexico_ITRF2008",DATUM["Mexico_ITRF2008",SPHEROID["GRS_1980",6378137,298.257222101],TOWGS84[0,0,0,0,0,0,0]],PRIMEM["Greenwich",0],UNIT["Degree",0.0174532925199433]],PROJECTION["Lambert_Conformal_Conic_2SP",AUTHORITY["EPSG","9802"]],PARAMETER["Central_Meridian",-102],PARAMETER["Latitude_Of_Origin",12],PARAMETER["False_Easting",2500000],PARAMETER["False_Northing",0],PARAMETER["Standard_Parallel_1",17.5],PARAMETER["Standard_Parallel_2",29.5],PARAMETER["Scale_Factor",1],UNIT["Meter",1,AUTHORITY["EPSG","9001"]],AUTHORITY["EPSG","6372"]]')
     comerciosB_SHP = comerciosB_SHP.to_crs("EPSG:4326")
 
-    m3 = folium.Map(location=[lat1,lon1], zoom_start=13)
+    # Mapa con folum
+    map_html = getfolium_map(lat1, lon1, distancia_km, codigo_postal, comercios_SHP, comerciosB_SHP)
 
-    folium.Choropleth(
-    geo_data=comercios_SHP.to_json(drop_id=True),
-    name="Comercios",
-    data=comercios_SHP,
-    columns=["CVEGEO", "n_Comercios"],
-    key_on="feature.properties.CVEGEO",
-    fill_color="Spectral",
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name="Comercios",
-    ).add_to(m3)
-
-    folium.Circle(
-        location=[lat1, lon1],
-        radius=distancia_km * 1000,  # Radio en metros
-        color='blue',
-        fill=True,
-        fill_color='blue'
-        ).add_to(m3)
-
-    #Icono de estrella para comercios buscados
+    # Mapa con google maps
+    markers_comercios = "["
     for _, registro in comercios_SHP.iterrows():
-        folium.Marker([registro["latitud"], registro["longitud"]], popup=registro["nom_estab"], icon=folium.Icon(color="blue", icon="star")).add_to(m3)
+        markers_comercios=markers_comercios + '{lat: ' + str(registro["latitud"]) + ", lng:" + str(registro["longitud"]) + ", title:'" + str(registro["nom_estab"]) + "', label:'" + str(registro["cod_postal"])+"'},"
+    
+    markers_comercios = markers_comercios[:-1]
+    markers_comercios = markers_comercios + "]"
 
-    #Icono de warning para competidores
+    # print(markers_comercios)
+
+    markers_competencia = "["
     for _, registro in comerciosB_SHP.iterrows():
-        folium.Marker([registro["latitud"], registro["longitud"]], popup=registro["nom_estab"], icon=folium.Icon(color="red", icon="warning-sign")).add_to(m3)
+        markers_competencia= markers_competencia + '{lat: ' + str(registro["latitud"]) + ", lng:" + str(registro["longitud"]) + ", title:'" + str(registro["nom_estab"]) + "', label:'" + str(registro["cod_postal"])+"'},"
+    
+    markers_competencia = markers_competencia[:-1]
+    markers_competencia = markers_competencia + "]"
 
-    #Icono de punto central del código postal buscado
-    folium.Marker([lat1, lon1],  popup= "C.P. " + codigo_postal, icon=folium.Icon(color="green", icon="info-sign")).add_to(m3)
+    #iteramos sobre negocios_radio
+    markers_comercios_radio = "["
+    for row in negocios_radio.collect(): # Use .collect() to get the data as a list of Row objects
+        markers_comercios_radio= markers_comercios_radio + '{lat: ' + str(row["Latitud"]) + ", lng:" + str(row["Longitud"]) + ", title:'CP " + str(row["CP"]) + "', label:'CP " + str(row["CP"])+"'},"
+    
+    markers_comercios_radio = markers_comercios_radio[:-1]
+    markers_comercios_radio = markers_comercios_radio + "]"
 
-    map_html = m3._repr_html_()
 
     # Convertir a una lista de diccionarios para pasar a la plantilla
     codigo_buscado_data = codigo_buscado.to_dict(orient='records')
@@ -256,7 +250,14 @@ def Get_actividades(request,codigo_postal, radio, actividad_to_search):
         'Message': 'Operacion Exitosa!',
           'codigo_buscado_data':codigo_buscado_data, 
           'radio':radio,
-          'map_html': map_html
+          'map_html':map_html
+        #   'map_html': {
+        #       'lat1':lat1,
+        #       'lon1':lon1,
+        #       'markers_comercios':markers_comercios,
+        #       'markers_competencia':markers_competencia,
+        #       'markers_comercios_radio':markers_comercios_radio
+        #   }
     }
     return JsonResponse(data)
 
@@ -286,3 +287,40 @@ def load_parquetby_estado(estados):
                 BD_DENUE = BD_DENUE.union(BD_DENUE1)
                 
     return BD_DENUE
+
+
+def getfolium_map(lat_center,long_center,distance_km, postal_code,comercios_radio, comercios_competidores):
+    m3 = folium.Map(location=[lat_center,long_center], zoom_start=13)
+
+    folium.Choropleth(
+    geo_data=comercios_radio.to_json(drop_id=True),
+    name="Comercios",
+    data=comercios_radio,
+    columns=["CVEGEO", "n_Comercios"],
+    key_on="feature.properties.CVEGEO",
+    fill_color="Spectral",
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name="Comercios",
+    ).add_to(m3)
+
+    folium.Circle(
+        location=[lat_center, long_center],
+        radius=distance_km * 1000,  # Radio en metros
+        color='blue',
+        fill=True,
+        fill_color='blue'
+        ).add_to(m3)
+
+    #Icono de estrella para comercios buscados
+    for _, registro in comercios_radio.iterrows():
+        folium.Marker([registro["latitud"], registro["longitud"]], popup=registro["nom_estab"], icon=folium.Icon(color="blue", icon="star")).add_to(m3)
+
+    #Icono de warning para competidores
+    for _, registro in comercios_competidores.iterrows():
+        folium.Marker([registro["latitud"], registro["longitud"]], popup=registro["nom_estab"], icon=folium.Icon(color="red", icon="warning-sign")).add_to(m3)
+
+    #Icono de punto central del código postal buscado
+    folium.Marker([lat_center, long_center],  popup= "C.P. " + postal_code, icon=folium.Icon(color="green", icon="info-sign")).add_to(m3)
+
+    return m3._repr_html_()
